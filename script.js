69908e031b1a48e5749cb130d61bcd74de30ca7b
GersonArlindo <gerson50039@gmail.com>
2023-03-25 17:40:25 -0600

    Version 1 de app Terricolas a la tierra

---
+++ script.js
@@ -0,0 +1,45 @@
+const LIMITE_INFERIOR = 10000
+
+function obtenerDatosUsuario() {
+    const nombre = document.getElementById("nombre").value;
+    const numeroDePlaneta = parseInt(document.getElementById("numeroPlaneta").value);
+    if (isNaN(numeroDePlaneta)) {
+      throw new Error("El número del planeta ingresado no es válido");
+    }
+    return { nombre, numeroDePlaneta };
+  }
+  
+  function saltosHastaPlaneta(numeroDePlaneta) {
+    if (numeroDePlaneta <= LIMITE_INFERIOR) {
+      throw new Error(
+        "Lo siento, el numero de planeta tiene que ser mayor a 10000"
+      );
+    }
+  
+    let saltosTotales = 0;
+    while (numeroDePlaneta !== 1) {
+      numeroDePlaneta % 2 === 0
+        ? (numeroDePlaneta /= 2)
+        : (numeroDePlaneta = numeroDePlaneta * 3 + 1);
+      saltosTotales++;
+    }
+    return `Número total de saltos: ${saltosTotales}`;
+  }
+  
+  function manejarError(error) {
+    alert(error.message);
+  }
+  
+  function manejarEnvioFormulario(event) {
+    event.preventDefault();
+    try {
+      const { nombreUsuario, numeroDePlaneta } = obtenerDatosUsuario();
+      const resultado = saltosHastaPlaneta(numeroDePlaneta);
+      document.getElementById("resultado").innerHTML = `${nombreUsuario}, ${resultado}`;
+    } catch (error) {
+      manejarError(error);
+    }
+  }
+  
+  document.getElementById("formulario").addEventListener("submit", manejarEnvioFormulario);
+  
